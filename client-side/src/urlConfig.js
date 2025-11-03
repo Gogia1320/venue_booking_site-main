17367878b8f0e4a9d212d7cac63b627a3b2cbdc4
Paras Gogia <parasgogia4@gmail.com>
2025-11-03 16:29:11 +0530

    fifth commit

--- client-side/src/urlConfig.js
+++ client-side/src/urlConfig.js
@@ -1,4 +1,4 @@
-export const api = 'https://venue-booking-site-backend-lqiu.onrender.com/api';
+export const api = 'https://venue-backend1.onrender.com/api';
 export const getPublicURL = (filename) => {
-    return `https://venue-booking-site-backend-lqiu.onrender.com/public/${filename}`;
+    return `https://venue-backend1.onrender.com/public/${filename}`;
 }
